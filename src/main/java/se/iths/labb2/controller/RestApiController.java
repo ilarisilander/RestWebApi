ae3b6c5c1746b690dfbf151f94d88afd97a85830
Ilari Silander <ilarisilander@hotmail.com>
2019-03-17 15:33:24 +0100

    Add POST Game Method

--- src/main/java/se/iths/labb2/controller/RestApiController.java
+++ src/main/java/se/iths/labb2/controller/RestApiController.java
@@ -3,12 +3,11 @@ package se.iths.labb2.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
@@ -32,6 +31,7 @@ public class RestApiController {
 
     //GET A Single Game
 
+    @RequestMapping(value = "/game/{id}", method = RequestMethod.POST)
     public ResponseEntity<?> getGame(@PathVariable("id") long id) {
         logger.info("Get Game with ID {}", id);
         Game game = gameService.findById(id);
@@ -41,6 +41,24 @@ public class RestApiController {
                 + " not found"), HttpStatus.OK);
         }
         return new ResponseEntity<Game>(game, HttpStatus.OK);
+    }
+
+    //POST A Game
+
+    @RequestMapping(value = "/game/", method = RequestMethod.POST)
+    public ResponseEntity<?> createGame(@RequestBody Game game, UriComponentsBuilder ucBuilder) {
+        logger.info("Creating Game : {}", game);
+
+        if(gameService.isGameExist(game)) {
+            logger.error("Unable to create. A Game with name {} already exists", game.getName());
+            return new ResponseEntity(new CustomErrorType("Unable to create. A Game with name " +
+                    game.getName() + " already exist."), HttpStatus.CONFLICT);
+        }
+        gameService.saveGame(game);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/game/{id}").buildAndExpand(game.getId()).toUri());
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
     }
 
 }
