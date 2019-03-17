9ff555223acaaf523d577e2d4a981daf0062458f
Ilari Silander <ilarisilander@hotmail.com>
2019-03-17 14:17:30 +0100

    Add GET All Method

--- src/main/java/se/iths/labb2/controller/RestApiController.java
+++ src/main/java/se/iths/labb2/controller/RestApiController.java
@@ -1,4 +1,34 @@
 package se.iths.labb2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+    public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
+
+    @Autowired
+    GameService gameService;
+
+    //GET All Games
+
+    @RequestMapping(value = "/game/", method = RequestMethod.GET)
+    public ResponseEntity<List<Game>> listAllGames() {
+        List<Game> games = gameService.findAllGames();
+        if(games.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Game>>(games, HttpStatus.OK);
+    }
+
+
+
 }
