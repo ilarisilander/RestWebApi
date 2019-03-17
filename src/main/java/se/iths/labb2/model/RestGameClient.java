467dfb318f0caabad7a100bc429ec4c347dd075f
Ilari Silander <ilarisilander@hotmail.com>
2019-03-17 23:31:09 +0100

    Change URI for Test Client

--- src/main/java/se/iths/labb2/model/RestGameClient.java
+++ src/main/java/se/iths/labb2/model/RestGameClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class RestGameClient {
 
-    public static final String REST_SERVICE_URI = "http://localhost:8080/SpringBootRestApi/api";
+    public static final String REST_SERVICE_URI = "http://localhost:8080/api/";
 
     //GET
     @SuppressWarnings("unchecked")
